9b317af6ca525216a7835a643d167c79649e28c9
Joshua MIraglia <joshuamiraglia@gmail.com>
2021-01-20 08:22:03 -0800

    added the M5 node

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-<<<<<<< HEAD
+
 # install ros2
 ./install_foxy_debian.sh
 
@@ -8,7 +8,3 @@
 curl -sSL http://get.gazebosim.org | sh
 sudo apt install ros-foxy-gazebo-ros-pkgs
 
-=======
-#install ros2 foxy
-./install_foxy_debian.sh
->>>>>>> master
